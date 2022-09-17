57beefb36c3cd0b1ccd85c49d091b78358de6fe6
killuanoxius <108164826+killuanoxius@users.noreply.github.com>
2022-09-18 00:24:39 +0800

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -422,14 +422,14 @@ def emiko_about_callback(update, context):
         )
     elif query.data == "emiko_support":
         query.message.edit_text(
-            text="*๏ Emiko support chats*"
+            text="*๏ Maternal support chats*"
             f"\nJoin My Support Group/Channel for see or report a problem on {dispatcher.bot.first_name}.",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="Support", url="t.me/emikosupport"),
-                    InlineKeyboardButton(text="Updates", url="https://t.me/KennedyProject"),
+                    InlineKeyboardButton(text="Support", url="https://t.me/withyohaneshere"),
+                    InlineKeyboardButton(text="Updates", url="https://t.me/withyohaneshere"),
                  ],
                  [
                     InlineKeyboardButton(text="Go Back", callback_data="emiko_"),
